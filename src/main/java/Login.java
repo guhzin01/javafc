28b07aab64ea9100e4778d520d9e375480996915
guhzin01 <guhzindev@gmail.com>
2026-05-26 09:55:30 -0300

    Primeiro commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,5 @@
+public class Login {
+    public boolean autenticar(String usuario, String senha) {
+        return "admin".equals(usuario) && "123".equals(senha);
+    }
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginTest {
+    @Test
+    public void deveAutenticarLoginCorreto() {
+        Login login = new Login();
+
+        assertTrue(login.autenticar("admin", "123"));
+    }
+
+    @Test
+    public void naoDeveAutenticarLoginIncorreto() {
+        Login login = new Login();
+
+        assertFalse(login.autenticar("usuario", "senha"));
+    }
+}
